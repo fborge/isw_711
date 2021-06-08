a3fe780574fcf797b72772c510fa77715219f62e
fborges.v6 <fborges.v6@gmail.com>
2021-06-07 20:30:04 -0600

    Define delete method.

--- workshop-3/course-api-rest/client/src/app/course/course/course.component.ts
+++ workshop-3/course-api-rest/client/src/app/course/course/course.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CourseService} from 'src/app/service/course.service';
+import { ToastrService} from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-course',
@@ -8,7 +10,8 @@ import {CourseService} from 'src/app/service/course.service';
 })
 export class CourseComponent implements OnInit {
   courses:any;
-  constructor(private courseService:CourseService) { }
+  data:any;
+  constructor(private courseService:CourseService, private toastr:ToastrService) { }
 
   ngOnInit(): void {
     this.getCourseData();
@@ -18,5 +21,17 @@ export class CourseComponent implements OnInit {
     this.courseService.getData().subscribe(res=>{
       this.courses=res;
     })
+  }
+
+  deleteData(id:any){
+    this.courseService.deleteData(id).subscribe(res =>{
+      this.data =res;
+      this.toastr.error(JSON.stringify(this.data.name), JSON.stringify(this.data.message),
+      {
+        timeOut:1000,
+        progressBar:true
+      });
+      this.getCourseData();
+    });
   }
 }
